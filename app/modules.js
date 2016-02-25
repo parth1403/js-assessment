068f9fa228822d444e889c6d98854fde8afba411
Parth Suthar <pvs1403@gmail.com>
2016-02-25 11:58:14 +0530

    Update modules.js

--- app/modules.js
+++ app/modules.js
@@ -3,5 +3,19 @@ exports = (typeof window === 'undefined') ? global : window;
 exports.modulesAnswers = {
   createModule : function(str1, str2) {
 
+  	var module = (function(greeting, name){
+
+  		var singleton = {};
+  		singleton.name = name;
+  		singleton.greeting = greeting;
+  		
+  		singleton.sayIt = function sayIt (argument) {
+  			return singleton.greeting + ", " + singleton.name;
+  		};
+
+  		return singleton;
+  	})(str1, str2);
+
+  	return module;
   }
 };
