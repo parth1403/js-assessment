b90e4ec5051d751ac4298ac5f67682785cbdb50d
Parth Suthar <pvs1403@gmail.com>
2016-02-25 11:57:01 +0530

    Update count.js

--- app/count.js
+++ app/count.js
@@ -2,6 +2,21 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.countAnswers =  {
   count : function (start, end) {
-
+  	var counter = [start], curCount = start;
+  	console.log(curCount);
+  	var intervalId = setInterval(function(){
+  		curCount++;
+  		if(curCount <= end){
+  			counter.push(curCount);	
+  			console.log(curCount);
+  		}else{
+  			counter.cancel();
+  		}
+  		
+  	},100);
+  	counter.cancel = function(){
+  		clearInterval(intervalId);
+  	}
+  	return counter;
   }
 };
