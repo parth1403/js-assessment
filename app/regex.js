4227a4cc4c47f08bcf98552f1df03ab244e2b1ff
Parth Suthar <pvs1403@gmail.com>
2016-02-25 11:59:09 +0530

    Update regex.js

--- app/regex.js
+++ app/regex.js
@@ -2,25 +2,36 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.regexAnswers = {
   containsNumber : function(str) {
-
+    var reg = /[0-9]/;
+    return reg.test(str);
   },
 
   containsRepeatingLetter : function(str) {
-
+    var reg = /([a-zA-Z])\1/;
+    return reg.test(str);
   },
 
   endsWithVowel : function(str) {
-
+    var reg = /[AEIOUaeiou]$/;
+    return reg.test(str);
   },
 
   captureThreeNumbers : function(str) {
-
+    var reg = /[0-9]{3}/;
+    var result = reg.exec(str);
+    if(result == null){
+      return false;
+    }else{
+      return result[0];
+    }
   },
 
   matchesPattern : function(str) {
-
+    var reg = /^[0-9]{3}-[0-9]{3}-[0-9]{4}$/;
+    return reg.test(str);
   },
   isUSD : function(str) {
-
+    var reg = /^\$[0-9]+(,[0-9]{3})*(.[0-9]{2}){0,1}$/;
+    return reg.test(str);
   }
 };
