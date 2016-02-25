82c7dae317b1d2002a79d05217e3116dd5061cc4
Parth Suthar <pvs1403@gmail.com>
2016-02-25 11:58:57 +0530

    Update recursion.js

--- app/recursion.js
+++ app/recursion.js
@@ -2,7 +2,13 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.recursionAnswers = {
   listFiles: function(data, dirName) {
-
+  	var files = [];
+  	function appendFiles(data, dirName){
+  		// for(var files){
+        
+    //   }
+  	}
+  	return appendFiles(data, dirName);
   },
 
   permute: function(arr) {
