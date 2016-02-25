2d41776d580119876f6fbbb0dbc63bb960c07f3e
Parth Suthar <pvs1403@gmail.com>
2016-02-25 11:58:29 +0530

    Update numbers.js

--- app/numbers.js
+++ app/numbers.js
@@ -2,18 +2,25 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.numbersAnswers = {
   valueAtBit: function(num, bit) {
-
+  	var bStr = num.toString(2);
+  	return parseInt(bStr[bStr.length - bit]);
   },
 
   base10: function(str) {
-
+  	return parseInt(str,2)
   },
 
   convertToBinary: function(num) {
-
+  	var binary = num.toString(2);
+  	while(binary.length < 8){
+  		binary = '0' + binary;
+  	}
+  	return binary;
   },
 
   multiply: function(a, b) {
-
+  	var bStr = b.toString();
+  	var precision = bStr.length - bStr.indexOf(".") - 1;
+  	return parseFloat((a * b).toFixed(precision));
   }
 };
