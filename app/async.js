086c2348d7c27aff570d0aef7c10379f3112405e
Parth Suthar <pvs1403@gmail.com>
2016-02-25 11:56:31 +0530

    Update async.js

--- app/async.js
+++ app/async.js
@@ -2,10 +2,33 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.asyncAnswers = {
   async : function(value) {
-
+  	var promise = new Promise(function(resolve, reject){
+  		setTimeout(function(){
+  			resolve(value);
+  		}, 100);
+  	});
+  	return promise;
   },
 
   manipulateRemoteData : function(url) {
-
+  	var promise = new Promise(function(resolve, reject){
+  		var xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = function () {
+		    var DONE = this.DONE || 4;
+		    if (this.readyState === DONE){
+		        var data = JSON.parse(this.responseText);
+		        var people = data.people;
+		        var peoplenames = [];
+		        for(var i  = 0; i < people.length; i++){
+		        	peoplenames.push(people[i].name);
+		        }
+		        resolve(peoplenames);
+		    }
+		};
+		xhr.open('GET', url, true);
+		xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+		xhr.send(null);
+  	});
+  	return promise;
   }
 };
